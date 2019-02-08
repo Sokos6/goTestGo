5ecc9b757fc78f24e4e12576ec63bfa8a5c1527a
Will Sokolowski <will.sokolowski@gmail.com>
2019-02-08 11:29:10 -0700

    sum and working test

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,11 @@
 package integers
 
-import "testing"
+// godoc -http=:6060 and navigate to http://localhost:6060/pkg/
+
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,4 +14,10 @@ func TestAdder(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
